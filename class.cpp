66fb9d4980e63ecb5454bfee46ac0cbede35bc84
Saksham Arya <76654066+Shane-White1308@users.noreply.github.com>
2022-05-24 11:15:43 +0530

    Update class.cpp

--- class.cpp
+++ class.cpp
@@ -1,43 +1,13 @@
 #include <iostream>
 using namespace std;
 
-class Mammals
+void add(int a, int b)
 {
-    public:
-    void Mammal()
-    {
-        cout<<"I am mammal"<<endl;
-    }
-};
-
-class Marineanimals
-{
-    public:
-    void Marine()
-    {
-        cout<<"I am marine animal"<<endl;
-    }
-};
-
-class BlueWhale: public Mammals, public Marineanimals
-{
-    public:
-    void blue()
-    {
-        cout<<"I belong to both categories: Mammals as well as marine animals"<<endl;
-    }
-};
+    cout<<a+b<<endl;
+}
 
 int main()
 {
-    Mammals obj;
-    obj.Mammal();
-
-    Marineanimals obj1;
-    obj1.Marine();
-
-    BlueWhale obj3;
-    obj3.blue();
-    obj3.Mammal();
-    obj3.Marine();
+    add(10,20);
+    return 0;
 }
